949c0f6d159adf82776ea9b66d27d4cb2145e041
Benn9211 <bhanupartaps38@gmail.com>
2016-12-09 01:56:10 -0800

    Movie trailer Project
    open only the entertainment_center.py file to see the final result

---
+++ bangbang.py
@@ -0,0 +1,8 @@
+import media
+
+bang_bang = media.Movie("Bang Bang",
+                        "A thief that stole the kuhinoor",
+                        "https://en.wikipedia.org/wiki/Bang_Bang!#/media/File:Bang_Bang_(2014_Film).jpg",
+                        "https://www.youtube.com/watch?v=LRARHtMzZQE")
+print(bang_bang.storyline)
+bang_bang.show_trailer()
---
+++ entertainment_center.py
@@ -0,0 +1,37 @@
+import fresh_tomatoes
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A Story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=ZZv1vki4ou4")
+#print(toy_story.storyline)
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+#print(avatar.storyline)
+#avatar.show_trailer()
+xXx_return_of_xander_cage = media.Movie("xXx: Return of Xander Cage",
+                        "This will also mark the first film produced",
+                        "https://upload.wikimedia.org/wikipedia/en/7/73/XXx_Return_of_Xander_Cage_offcial_logo_poster.jpg",
+                        "https://www.youtube.com/watch?v=MQEFmHsseaU")
+
+bad_moms = media.Movie("Bad Moms",
+                     "Three lady try to become a good moms",
+                     "https://upload.wikimedia.org/wikipedia/en/7/70/Bad_Moms_poster.jpg",
+                     "https://www.youtube.com/watch?v=iKCw-kqo3cs")
+
+bang_bang = media.Movie("Bang Bang",
+                        "A thief that stole the kuhinoor",
+                        "https://upload.wikimedia.org/wikipedia/en/9/90/Bang_Bang_%282014_Film%29.jpg",
+                        "https://www.youtube.com/watch?v=LRARHtMzZQE")
+doctor_strange = media.Movie("Doctor Strange",
+                        "A doctor that leans how to fix  his broken hands",
+                        "https://upload.wikimedia.org/wikipedia/en/4/4f/Doctor_Strange_Vol_4_2_Ross_Variant_Textless.jpg",
+                        "https://www.youtube.com/watch?v=HSzx-zryEgM")
+
+movies = [toy_story, avatar, xXx_return_of_xander_cage, bad_moms, bang_bang,doctor_strange]
+fresh_tomatoes.open_movies_page(movies)
+print(media.Movie.VALID_RATINGS)
+print(media.Movie.__doc__)
